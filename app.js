c9518cb4b9be6a6c235528dcd12fe7c0eae5a187
D.Henry <lindong.yang@intchains.com>
2021-06-30 11:20:19 +0800

    feat: 项目添加nodemon热更新

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const expressHandler = require('express-handlebars');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
 
@@ -33,6 +33,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', indexRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const handlers = require('../lib/handlers');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -13,5 +14,9 @@ router.get('/foo', (req, res, next) => {
 router.get('/section-test', (req, res, next) => {
   res.render('section-test');
 });
+
+router.get('/newsletter-signup', handlers.newsletterSignup);
+router.post('/newsletter-signup/process', handlers.newsletterSignupProcess);
+router.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou);
 
 module.exports = router;
